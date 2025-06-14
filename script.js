ae5db0111733499cea030ea6af8dfdd999cd03b9
MatveiKow4eg <bonantik09@gmail.com>
2025-06-14 21:17:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const db = firebase.database();
 function formatTime(seconds) {
   const m = Math.floor(seconds / 60).toString().padStart(2, "0");
   const s = (seconds % 60).toString().padStart(2, "0");
-  return \`\${m}:\${s}\`;
+  return `${m}:${s}`;
 }
 
 // --- Для страницы участника ---
